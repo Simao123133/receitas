1bde058ed33fa098c75e0e85d8c3883161baa540
simao amaro <simaoamaro2000@gmail.com>
2022-04-15 18:37:00 +0100

    new

--- receitas/settings.py
+++ receitas/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-n*i&te!!6us#ft5_!$zge)zo&v(u%((-thg@b!fe@0n9#5%d!g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
