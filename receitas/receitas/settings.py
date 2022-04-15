cfe3dc33d9f0fbfb816d557e832a79370d52e7c7
simao amaro <simaoamaro2000@gmail.com>
2022-04-15 15:03:11 +0100

    hello

--- receitas/receitas/settings.py
+++ receitas/receitas/settings.py
@@ -81,8 +81,8 @@ WSGI_APPLICATION = 'receitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'ciba',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_sqlite3')
     }
 }
 
